bf1a6863633f4a0352d673f9ba3dcd4f3b75a56e
adamsaleh <adamsaleh797@gmail.com>
2024-06-03 15:01:35 +0900

    combines dog gif with moon phase display info

--- app/src/main/java/com/example/werewolf/presentation/MainActivity.kt
+++ app/src/main/java/com/example/werewolf/presentation/MainActivity.kt
@@ -7,7 +7,6 @@
 package com.example.werewolf.presentation
 
 import android.os.Bundle
-import android.view.Display
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -15,6 +14,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,6 +23,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
@@ -43,13 +45,13 @@ class MainActivity : ComponentActivity() {
         setTheme(android.R.style.Theme_DeviceDefault)
 
         setContent {
-            WearApp("Android")
+            WearApp()
         }
     }
 }
 
 @Composable
-fun WearApp(greetingName: String) {
+fun WearApp() {
     WerewolfTheme {
         Box(
             modifier = Modifier
@@ -58,16 +60,20 @@ fun WearApp(greetingName: String) {
             contentAlignment = Alignment.Center
         ) {
             TimeText()
-            DisplayGif(modifier = Modifier)
+            MoonPhaseText(modifier = Modifier.align(Alignment.TopCenter).offset(0.dp, 40.dp))
+            DisplayGif(modifier = Modifier
+                .size(110.dp)
+                .align(Alignment.BottomCenter)
+                .offset(0.dp, (-10).dp))
 
         }
     }
 }
 
 @Composable
-fun Greeting(greetingName: String) {
+fun MoonPhaseText(modifier: Modifier) {
     Text(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = modifier.fillMaxWidth(),
         textAlign = TextAlign.Center,
         color = MaterialTheme.colors.primary,
         text = moonPhase()
@@ -98,5 +104,5 @@ fun DisplayGif(
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    WearApp("Preview Android")
+    WearApp()
 }
